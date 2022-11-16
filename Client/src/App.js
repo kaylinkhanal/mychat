ecd799fa96c1ff6e1247ab8e811b84b0efdccc71
gautamyogesh017 <gautamyogesh017@.github.com>
2022-11-16 17:56:10 +0545

    added routing in register

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,18 +1,13 @@
-import Login from "./pages/Auth/Login";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
-import Register from "./pages/Auth/Register";
-import './App.css'
+import ChatUI from "./UI/chatUI";
+import { Route, Routes } from "react-router-dom";
+//import Register from "./pages/Auth/Register";
+import "./App.css";
 const App = () => {
   return (
     <div className="container">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<ChatUI />} />
+        {/* <Route path="/register" element={<Register />} /> */}
       </Routes>
     </div>
   );
