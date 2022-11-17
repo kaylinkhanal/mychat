8206896a5cbf3fe228bff3cb096640a0d9928f7c
rnimisha <nimisaraut@gmail.com>
2022-11-17 16:32:36 +0545

    hash password before saving

--- Server/src/Controller/registerRouter.js
+++ Server/src/Controller/registerRouter.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt')
 const users=require('../Model/usersSchema')
 
 // post request for register the user
 router.post("/", async (req, res) => {
-  console.log(req.body)
+
   try{
-    const data = await users.create(req.body)
-    if(data){
-      res.status(200).json({msg: "user registered"})
-    }
+    bcrypt.hash(req.body.password, 10, async function (err, hash) {
+      if (err) {
+        console.log(err.message)
+        res.status(403).json({
+          success: false,
+          error: err.message
+        })
+      } else {
+        req.body.password = hash
+        const data = await users.create(req.body)
+        if(data){
+          res.status(200).json({msg: "user registered"})
+        }
+      }
+    })
   }catch(err){
     console.log("err--", err.message)
   }
